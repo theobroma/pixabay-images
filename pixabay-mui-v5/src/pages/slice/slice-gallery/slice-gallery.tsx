f7b1f896f92973b71bb4d99fe266841e69c53611
Alexandr Siryj <theobroma222@gmail.com>
2023-01-04 10:29:16 +0200

    SliceGallery

---
+++ pixabay-mui-v5/src/pages/slice/slice-gallery/slice-gallery.tsx
@@ -0,0 +1,50 @@
+import { useEffect } from 'react';
+
+import { Grid } from '@mui/material';
+
+import { AppError } from '@/atoms/AppError/AppError';
+import { PageLoader } from '@/atoms/page-loader/page-loader';
+import { ImageGallery } from '@/components/ImageGallery/ImageGallery';
+import { useNonInitialEffect } from '@/hooks/useNonInitialEffect';
+import { useAppDispatch, useAppSelector } from '@/store/configureStore';
+import { picturesSelector } from '@/store/pictures/selectors';
+import { getPicturesTC } from '@/store/pictures/slice';
+
+export const SliceGallery = () => {
+  const dispatch = useAppDispatch();
+  const {
+    data: { hits },
+    error,
+    isError,
+    isLoading,
+    // isSuccess,
+  } = useAppSelector(picturesSelector);
+
+  useNonInitialEffect(() => {
+    if (hits.length === 0) {
+      dispatch(getPicturesTC({ page: 1 }));
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: 'smooth',
+    });
+  }, [hits]);
+
+  return (
+    <>
+      {isLoading && <PageLoader />}
+      {!!hits && <ImageGallery hits={hits} />}
+      <Grid container spacing={2}>
+        {/* error */}
+        {isError && (
+          <Grid item xs={12}>
+            <AppError error={error} />
+          </Grid>
+        )}
+      </Grid>
+    </>
+  );
+};
--- pixabay-mui-v5/src/pages/slice/slice.page.tsx
+++ pixabay-mui-v5/src/pages/slice/slice.page.tsx
@@ -1,58 +1,18 @@
-import { useEffect } from 'react';
+import { Container, Typography } from '@mui/material';
 
-import { Container, Grid, Typography } from '@mui/material';
-
-import { AppError } from '@/atoms/AppError/AppError';
-import { PageLoader } from '@/atoms/page-loader/page-loader';
 import { AppSearch } from '@/components/AppSearch/AppSearch';
-import { ImageGallery } from '@/components/ImageGallery/ImageGallery';
-import { useNonInitialEffect } from '@/hooks/useNonInitialEffect';
-import { useAppDispatch, useAppSelector } from '@/store/configureStore';
-import { picturesSelector } from '@/store/pictures/selectors';
-import { getPicturesTC } from '@/store/pictures/slice';
 
-const SlicePage = () => {
-  const dispatch = useAppDispatch();
-  const {
-    data: { hits },
-    error,
-    isError,
-    isLoading,
-    // isSuccess,
-  } = useAppSelector(picturesSelector);
+import { SliceGallery } from './slice-gallery/slice-gallery';
 
-  useNonInitialEffect(() => {
-    if (hits.length === 0) {
-      dispatch(getPicturesTC({ page: 1 }));
-    }
-  }, [dispatch]);
-
-  useEffect(() => {
-    window.scrollTo({
-      top: document.documentElement.scrollHeight,
-      behavior: 'smooth',
-    });
-  }, [hits]);
-
-  return (
-    <Container maxWidth="lg">
-      <Typography variant="h5" component="h2">
-        Slice Version
-      </Typography>
-      <AppSearch />
-      {isLoading && <PageLoader />}
-      {!!hits && <ImageGallery hits={hits} />}
-      <Grid container spacing={2}>
-        {/* error */}
-        {isError && (
-          <Grid item xs={12}>
-            <AppError error={error} />
-          </Grid>
-        )}
-      </Grid>
-    </Container>
-  );
-};
+const SlicePage = () => (
+  <Container maxWidth="lg">
+    <Typography variant="h5" component="h2">
+      Slice Version
+    </Typography>
+    <AppSearch />
+    <SliceGallery />
+  </Container>
+);
 
 // eslint-disable-next-line import/no-default-export
 export default SlicePage;
