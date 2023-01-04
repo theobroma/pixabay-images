1c55a5d17a060d83f587f4a4d07b37b56becee8d
Alexandr Siryj <theobroma222@gmail.com>
2023-01-04 10:39:29 +0200

    RtkqGallery

---
+++ pixabay-mui-v5/src/pages/rtkq/rtkq-gallery/rtkq-gallery.tsx
@@ -0,0 +1,26 @@
+import { Grid } from '@mui/material';
+
+import { AppError } from '@/atoms/AppError/AppError';
+import { PageLoader } from '@/atoms/page-loader/page-loader';
+import { ImageGallery } from '@/components/ImageGallery/ImageGallery';
+import { useGetPicturesQuery } from '@/store/pictures/api';
+
+export const RtkqGallery = () => {
+  const { data, isError, isLoading, error } = useGetPicturesQuery(1);
+  const hits = data?.hits;
+
+  return (
+    <>
+      {isLoading && <PageLoader />}
+      {!!hits && <ImageGallery hits={hits} />}
+      <Grid container spacing={2}>
+        {/* error */}
+        {isError && (
+          <Grid item xs={12}>
+            <AppError error={error} />
+          </Grid>
+        )}
+      </Grid>
+    </>
+  );
+};
--- pixabay-mui-v5/src/pages/rtkq/rtkq.page.tsx
+++ pixabay-mui-v5/src/pages/rtkq/rtkq.page.tsx
@@ -1,34 +1,18 @@
-import { Container, Grid, Typography } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 
-import { AppError } from '@/atoms/AppError/AppError';
-import { PageLoader } from '@/atoms/page-loader/page-loader';
 import { AppSearch } from '@/components/AppSearch/AppSearch';
-import { ImageGallery } from '@/components/ImageGallery/ImageGallery';
-import { useGetPicturesQuery } from '@/store/pictures/api';
 
-const RTKQPage = () => {
-  const { data, isError, isLoading, error } = useGetPicturesQuery(1);
-  const hits = data?.hits;
+import { RtkqGallery } from './rtkq-gallery/rtkq-gallery';
 
-  return (
-    <Container maxWidth="lg">
-      <Typography variant="h5" component="h2">
-        RTKQPage
-      </Typography>
-      <AppSearch />
-      {isLoading && <PageLoader />}
-      {!!hits && <ImageGallery hits={hits} />}
-      <Grid container spacing={2}>
-        {/* error */}
-        {isError && (
-          <Grid item xs={12}>
-            <AppError error={error} />
-          </Grid>
-        )}
-      </Grid>
-    </Container>
-  );
-};
+const RTKQPage = () => (
+  <Container maxWidth="lg">
+    <Typography variant="h5" component="h2">
+      RTKQPage
+    </Typography>
+    <AppSearch />
+    <RtkqGallery />
+  </Container>
+);
 
 // eslint-disable-next-line import/no-default-export
 export default RTKQPage;
