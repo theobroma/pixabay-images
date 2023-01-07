f1f3e5c69e7996f52b5f06e68583062e6b2f29e7
Alexandr Siryj <theobroma222@gmail.com>
2023-01-07 12:58:45 +0200

    ImageGalleryItem

--- pixabay-mui-v5/src/components/ImageGallery/ImageGallery.tsx
+++ pixabay-mui-v5/src/components/ImageGallery/ImageGallery.tsx
@@ -1,10 +1,8 @@
-import InfoIcon from '@mui/icons-material/Info';
-import IconButton from '@mui/material/IconButton';
 import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
 
 import { HitsEntityType } from '@/types';
+
+import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 
 interface Props {
   hits: HitsEntityType[];
@@ -13,23 +11,7 @@ interface Props {
 export const ImageGallery = ({ hits = [] }: Props) => (
   <ImageList cols={5} rowHeight={164}>
     {hits.map((hit) => (
-      <ImageListItem key={hit.id}>
-        <img src={hit.webformatURL} alt={hit.tags} loading="lazy" />
-        <ImageListItemBar
-          title={hit.tags}
-          subtitle={hit.user}
-          actionIcon={
-            <IconButton
-              sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-              aria-label="info about"
-              href={hit.pageURL}
-              target="_blank"
-            >
-              <InfoIcon />
-            </IconButton>
-          }
-        />
-      </ImageListItem>
+      <ImageGalleryItem hit={hit} />
     ))}
   </ImageList>
 );
---
+++ pixabay-mui-v5/src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.tsx
@@ -0,0 +1,30 @@
+import InfoIcon from '@mui/icons-material/Info';
+import IconButton from '@mui/material/IconButton';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+
+import { HitsEntityType } from '@/types';
+
+interface Props {
+  hit: HitsEntityType;
+}
+
+export const ImageGalleryItem = ({ hit }: Props) => (
+  <ImageListItem key={hit.id}>
+    <img src={hit.webformatURL} alt={hit.tags} loading="lazy" />
+    <ImageListItemBar
+      title={hit.tags}
+      subtitle={hit.user}
+      actionIcon={
+        <IconButton
+          sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+          aria-label="info about"
+          href={hit.pageURL}
+          target="_blank"
+        >
+          <InfoIcon />
+        </IconButton>
+      }
+    />
+  </ImageListItem>
+);
