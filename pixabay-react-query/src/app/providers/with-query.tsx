9852fde302b77a903711e2eb8cfd4e30e2abc012
Alexandr Siryj <theobroma222@gmail.com>
2023-02-12 18:47:13 +0200

    react-query-devtools

--- pixabay-react-query/src/app/providers/with-query.tsx
+++ pixabay-react-query/src/app/providers/with-query.tsx
@@ -1,6 +1,12 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
 export const withQuery = (component: () => React.ReactNode) => () =>
-  <QueryClientProvider client={queryClient}>{component()}</QueryClientProvider>;
+  (
+    <QueryClientProvider client={queryClient}>
+      {component()}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
